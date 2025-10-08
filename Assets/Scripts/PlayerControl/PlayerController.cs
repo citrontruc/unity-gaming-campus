8d26556467049656e8ba66b9142fbf596d3f1ef6
clement lion <clement.lionceau@gmail.com>
2025-10-08 18:11:25 +0200

    restarted from the beginning to accomodate unity programming

--- Assets/Scripts/PlayerControl/PlayerController.cs
+++ Assets/Scripts/PlayerControl/PlayerController.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;  // 1. The Input System "using" statement
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    // 2. These variables are to hold the Action references
+    InputAction moveAction;
+    InputAction jumpAction;
 
-    // Update is called once per frame
-    void Update() { }
+    private void Start()
+    {
+        // 3. Find the references to the "Move" and "Jump" actions
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    void Update()
+    {
+        // 4. Read the "Move" action value, which is a 2D vector
+        // and the "Jump" action state, which is a boolean value
+
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        Debug.Log($"Move Value {moveValue}");
+        // your movement code here
+
+        if (jumpAction.IsPressed())
+        {
+            // your jump code here
+        }
+    }
 }
