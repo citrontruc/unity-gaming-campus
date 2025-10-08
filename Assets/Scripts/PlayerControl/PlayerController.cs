215a661f24274819de19f1624ec683cc6fb69610
clement lion <clement.lionceau@gmail.com>
2025-10-08 18:11:44 +0200

    pre-commit hook

--- Assets/Scripts/PlayerControl/PlayerController.cs
+++ Assets/Scripts/PlayerControl/PlayerController.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.InputSystem;  // 1. The Input System "using" statement
+using UnityEngine.InputSystem; // 1. The Input System "using" statement
 
 public class PlayerController : MonoBehaviour
 {
